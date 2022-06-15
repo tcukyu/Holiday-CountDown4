4ec6457153547e50fa1342263fee489635157113
tcukyu <101930693+tcukyu@users.noreply.github.com>
2022-06-15 12:05:56 +0000

    commit data

--- src/App.js
+++ src/App.js
@@ -2,4 +2,4 @@ import React, { Component } from "react";
 import Media from "react-bootstrap/Media";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
-import Button from "rea
+import Button from "react-bootstrap/Button"
