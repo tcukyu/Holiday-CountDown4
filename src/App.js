c72c1e7a67c8ebaacac0315b14c59e70fc9ef145
tcukyu <101930693+tcukyu@users.noreply.github.com>
2022-06-15 21:17:31 +0000

    Fixing the bugs

--- src/App.js
+++ src/App.js
@@ -4,4 +4,5 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import underscore from "underscore";
-import a
+import axios from "axios";
+import "./
