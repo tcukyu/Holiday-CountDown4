f7b1fee2ee8823acd080b4311518cf9d8c1552f1
tcukyu <101930693+tcukyu@users.noreply.github.com>
2022-06-15 12:06:08 +0000

    fixed unittest

--- src/App.js
+++ src/App.js
@@ -2,4 +2,5 @@ import React, { Component } from "react";
 import Media from "react-bootstrap/Media";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
+import underscor
