cd88b11980c25d34ca5954746dad1f38b18b5500
tcukyu <101930693+tcukyu@users.noreply.github.com>
2023-01-02 21:42:23 +0000

    Add a missing space

--- src/App.test.js
+++ src/App.test.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('d
+  const div = document.createElement('div');
+  ReactDOM.render(<Ap
