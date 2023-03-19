0edcba31e211784fc55c569c1f07916bc2c768e4
tcukyu <101930693+tcukyu@users.noreply.github.com>
2023-03-19 18:44:12 +0000

    Layout improvement

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work 
+// If you want your app to work offline and load faster,
